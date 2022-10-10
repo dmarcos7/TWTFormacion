6244a365ea58a1ca47b8638077ff73d12e5d080a
dmarcos7 <davidmarcosnatividad@gmail.com>
2022-10-10 14:24:50 +0200

    cambios en el constructor

--- Proy001_JavaSE/src/main/java/com/curso/modulo6/Ingeniero.java
+++ Proy001_JavaSE/src/main/java/com/curso/modulo6/Ingeniero.java
@@ -1,5 +1,12 @@
 package com.curso.modulo6;
 
 public class Ingeniero extends Empleado {
-
+	public Ingeniero() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String getDetails() {
+		// TODO Auto-generated method stub
+		return super.getDetails();
+	}
 }
--- Proy001_JavaSE/src/main/java/com/curso/modulo6/Secretario.java
+++ Proy001_JavaSE/src/main/java/com/curso/modulo6/Secretario.java
@@ -2,4 +2,13 @@ package com.curso.modulo6;
 
 public class Secretario extends Empleado {
 
+	
+	public Secretario() {
+		
+	}
+	
+	@Override
+	public String getDetails() {
+		return super.getDetails();
+	}
 }
