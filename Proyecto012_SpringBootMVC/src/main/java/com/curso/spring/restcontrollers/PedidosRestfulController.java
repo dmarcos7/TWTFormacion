0d7551b0c4a0ae3087b6939500ca4d1179817ad5
dmarcos7 <davidmarcosnatividad@gmail.com>
2022-11-09 11:42:03 +0100

    CORS

--- Proyecto012_SpringBootMVC/src/main/java/com/curso/spring/restcontrollers/PedidosRestfulController.java
+++ Proyecto012_SpringBootMVC/src/main/java/com/curso/spring/restcontrollers/PedidosRestfulController.java
@@ -7,12 +7,14 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.spring.entidades.Pedido;
@@ -20,6 +22,12 @@ import com.curso.spring.excepciones.PedidosException;
 import com.curso.spring.servicios.PedidosService;
 
 @RestController
+@CrossOrigin(origins= {"http://localhost:4200"},
+methods= {RequestMethod.GET, 
+		RequestMethod.DELETE, 
+		RequestMethod.PUT,
+		RequestMethod.POST, 
+		RequestMethod.OPTIONS})
 public class PedidosRestfulController {
 	private static Logger log = LoggerFactory.getLogger(PedidosRestfulController.class);
 	
